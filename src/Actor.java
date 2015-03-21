a8d40333251d86d81689f0b6c02b59afae2583c1
rakesh <rickypharma@gmail.com>
2015-03-21 14:13:15 -0500

    initial commit

---
+++ src/Actor.java
@@ -0,0 +1,30 @@
+
+public class Actor {
+
+	public static void main(String[] args) {
+		//Actor actorChiru = new Actor();
+		//Actor actorRamcharan = new Actor();
+		
+		
+		
+		
+		Actor.act1();
+	}
+		
+		public void act(){
+			System.out.println("ACTING");
+			
+		}
+		
+		public static void act1(){
+			System.out.println("ACTING111111111111111111");
+			
+			
+			
+		}
+		
+	
+
+	}
+
+
